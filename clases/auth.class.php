8826fdeac94e834bc5f8384e1c2de5e8d159a60e
kevin7520 <kevin7520@hotmail.com>
2024-07-14 00:02:58 -0500

    Cambios

--- clases/auth.class.php
+++ clases/auth.class.php
@@ -1,7 +1,17 @@
 <?php
 require_once 'conexion/conexion.php';
 require_once 'conexion/respuestaGenerica.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+//Load Composer's autoloader
+//require 'vendor/autoload.php';
+require 'PHPMailer/Exception.php';
+require 'PHPMailer/PHPMailer.php';
+require 'PHPMailer/SMTP.php';
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
 class auth extends Conexion
 {
@@ -145,6 +155,34 @@ class auth extends Conexion
         $query = "UPDATE usuarios set nombres = '$nombre', apellidos = '$apellido', rol = '$rol', fechaNacimiento = '$fechaNacimiento', alias = '$usuario' where idUsuario = $id";
         return parent::nonQuery($query);
     }
+
+    // private function enviarDatosEmail($correo) {
+    //     try {
+    //         //Server settings
+    //         $mail->SMTPDebug = 0;                      //Enable verbose debug output
+    //         $mail->isSMTP();                                            //Send using SMTP
+    //         $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    //         $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+    //         $mail->Username = 'serious.game.ug@gmail.com';                     //SMTP username
+    //         $mail->Password = 'dkvakqmywddzgjnb';                               //SMTP password
+    //         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+    //         $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //         //Recipients
+    //         $mail->setFrom('serious.game.ug@gmail.com', 'ADMINISTRADOR');
+    //         $mail->addAddress('kaas7520@gmail.com');     //Add a recipient
+    //         //Content
+    //         $mail->isHTML(true);                                  //Set email format to HTML
+    //         $mail->Subject = 'Restablecer contraseña';
+    //         $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+    //         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    //         $mail->send();
+    //         echo 'Message has been sent';
+    //     } catch (Exception $e) {
+    //         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    //     }
+    // }
 
 
     private function insertarToken($usuarioid)
